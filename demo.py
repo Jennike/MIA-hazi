953c74f612ba420bf1bbcb514aefca9674af2380
SzmolyanJenni <szmolyanjenni22@gmail.com>
2021-12-02 19:29:22 +0100

    brach 3.feladat

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # Python program a páros/páratlan számok szétválasztásá
 # Egy szám páros, ha kettővel osztva a maradék 0
 # Ha a maradék 1, a szám páratlan
-
+#javítottam a párst párosra
 num = int(input("Adj meg egy számot: "))
 if (num % 2) == 0:
    print("{0} páros".format(num))
